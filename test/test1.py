36ce66646dda61f8fac7aa0ff678fe0f2f446d08
Arjan van der Velde <vandervelde.ag@gmail.com>
2017-08-21 20:32:01 -0400

    all cores

--- test/test1.py
+++ test/test1.py
@@ -12,18 +12,18 @@ def main():
 		print('Creating job template')
 		jt = s.createJobTemplate()
 		#jt.remoteCommand = os.path.join(os.getcwd(), 'slurm.sh')
-		jt.remoteCommand = "/usr/bin/env | grep SLURM"
+		jt.remoteCommand = "/usr/bin/env | grep SLURM; sleep 60"
 		jt.workingDirectory = drmaa.JobTemplate.HOME_DIRECTORY
 		#jt.outputPath =":"+drmaa.JobTemplate.HOME_DIRECTORY+'/DRMAA_JOB_OUT'
 		#jt.args = ['a', 'b']
 		#jt.joinFiles=True
 		jt.joinFiles=False
-		#jt.nativeSpecification = "--mincpus=1 --ntasks=0 --mem-per-cpu=10 --partition=zlurm"
+		jt.nativeSpecification = "--mincpus=1 --ntasks=0 --mem-per-cpu=10 --partition=zlurm"
 		# not specifiying resources will make it take a full host per job
 
 		jobid = 0
 		#jobid = s.runJob(jt)
-		jobid = s.runBulkJobs(jt, 1, 31, 1)
+		jobid = s.runBulkJobs(jt, 1, 372, 1)
 
 
 		print('Your job has been submitted with ID %s' % jobid)
